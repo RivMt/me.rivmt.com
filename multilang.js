25b37caefaf51faea4b98bc9f902687aea6c0527
San Kang <devrivmt@gmail.com>
2025-07-11 00:23:44 +0900

    Feat: Support automatic language selection

--- multilang.js
+++ multilang.js
@@ -1,9 +1,17 @@
 // Call updateContent() on page load
 window.addEventListener('DOMContentLoaded', async () => {
-    const userPreferredLanguage = localStorage.getItem('language') || 'en';
+    const userPreferredLanguage = loadLocale();
     const langData = await fetchLanguageData(userPreferredLanguage);
     updateContent(langData);
 });
+
+/**
+ * Load language preference
+ * @returns {string}
+ */
+async function loadLocale() {
+    return localStorage.getItem('language') || (navigator.language || navigator.userLanguage).split('-')[0] || 'en';
+}
 
 /**
  * Load language data
@@ -11,8 +19,27 @@ window.addEventListener('DOMContentLoaded', async () => {
  * @returns {Promise<any>}
  */
 async function fetchLanguageData(lang) {
-    const response = await fetch(`langs/${lang}.json`);
-    return response.json();
+    try {
+        const response = await fetch(`langs/${lang}.json`);
+        if (response.ok) {
+            return response.json(); // 성공 시 즉시 반환
+        }
+        if (lang !== 'en') {
+            console.warn(`Unable to find '${lang}.json'`);
+        }
+    } catch (error) {
+        console.error(`Unable to load '${lang}.json'`, error);
+    }
+
+    try {
+        const fallbackResponse = await fetch(`langs/en.json`);
+        if (fallbackResponse.ok) {
+            return fallbackResponse.json();
+        }
+    } catch (error) {
+        console.error(error);
+        return {};
+    }
 }
 
 /**
@@ -30,14 +57,17 @@ function setLanguagePreference(lang) {
  */
 function updateContent(langData) {
     document.querySelectorAll('[data-i18n]').forEach(element => {
-        const key = element.getAttribute('data-i18n');
-        const data = langData[key];
-        if (typeof data === 'string') {
-            element.textContent = langData[key];
-        } else if (data instanceof Array) {
-            let elements = element.getElementsByTagName('li')
-            for (let i = 0; i < elements.length; i++) {
-                elements[i].textContent = data[i];
+        const keyPath = element.getAttribute('data-i18n');
+        const keys = keyPath.split('.');
+        const value = keys.reduce((obj, key) => (obj && obj[key] !== undefined) ? obj[key] : undefined, langData);
+        if (typeof value === 'string') {
+            element.textContent = value;
+        } else if (Array.isArray(value)) {
+            const listItems = element.getElementsByTagName('li');
+            for (let i = 0; i < listItems.length; i++) {
+                if (value[i]) {
+                    listItems[i].textContent = value[i];
+                }
             }
         }
     });
